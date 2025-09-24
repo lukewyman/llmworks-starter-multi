b7726030cd245066a34e1f501101c8f4a400bb57
Luke Wyman <c-luke.wyman@charter.com>
2025-09-23 17:03:02 -0700

    Removed pulumi, going with Terragrunt/OpenTofu

--- infra/__main__.py
+++
@@ -1,7 +0,0 @@
-from importlib import import_module
-
-from pulumi import get_stack
-
-# Load the per-env program dynamically, e.g. stacks/dev/__main__.py
-stack = get_stack()  # "dev", "stg", "prod"
-import_module(f"stacks.{stack}.__main__")
